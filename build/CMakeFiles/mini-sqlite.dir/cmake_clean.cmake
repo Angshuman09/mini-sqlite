41396ababfb9854ca19bef73a4324319b8d2b4fc
Angshuman Kashyap <angshumankashyap69yap69@gmail.com>
2026-07-23 15:40:54 +0530

    set up CMake build

---
+++ build/CMakeFiles/mini-sqlite.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mini-sqlite.dir/src/input_buffer.c.o"
+  "CMakeFiles/mini-sqlite.dir/src/input_buffer.c.o.d"
+  "CMakeFiles/mini-sqlite.dir/src/main.c.o"
+  "CMakeFiles/mini-sqlite.dir/src/main.c.o.d"
+  "CMakeFiles/mini-sqlite.dir/src/meta_command.c.o"
+  "CMakeFiles/mini-sqlite.dir/src/meta_command.c.o.d"
+  "CMakeFiles/mini-sqlite.dir/src/statement.c.o"
+  "CMakeFiles/mini-sqlite.dir/src/statement.c.o.d"
+  "mini-sqlite"
+  "mini-sqlite.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mini-sqlite.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
